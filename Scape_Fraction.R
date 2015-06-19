8ad03a20c738c63a0dcb8525d253396ba2efc7be
Rafael S. de Souza <rafael.2706@gmail.com>
2015-06-19 10:42:21 +0200

    add references

--- Scape_Fraction.R
+++ Scape_Fraction.R
@@ -21,40 +21,69 @@ colnames(data.1)<-c("redshift","fEsc","Mvir","Mstar","Mgas","QHI","sfr_gas",
                     "sfr_stars","ssfr_gas","ssfr_stars","baryon_fraction",
                     "spin","age_star_mean","age_star_max","age_star_min")
 
+data.2<-data.1[data.1$redshift==15.20530,]
+data.3<-data.2[data.2$fEsc>0,]
 
-
-
-jags.data <- list(Y= data.1$fEsc,
-                  N = nrow(data.1),
-                  x=data.1$Mvir
+jags.data <- list(Y= data.3$fEsc,
+                  N = nrow(data.3),
+                  x=(data.3$Mgas-mean(data.3$Mgas))/sd(data.3$Mgas)
+#                  x=log(data.3$Mvir,10)
 )
 
 
 model<-"model{
 #1. Priors
-
-tau.R<-pow(sdBeta,-1)
-sdBeta ~ dgamma(0.001,0.001)
-#alpha1~dnorm(0,0.00001)
-#tau.z~dgamma(0.001,0.001)
-
-# Random intercept 
-for (j in 1:Ntype){
-ranef[j]~ddexp(0,tau.R)
-#ranef[j]~dnorm(alpha1,tau.z)
-}
-
-beta.0~dnorm(0,0.001)
-beta.1~dnorm(0,0.001)
-beta.2~dnorm(0,0.001)
-
+b0~dnorm(0,1e-5)
+b1~dnorm(0,1e-5)
+#c0~dnorm(0,1e-5)
+#c1~dnorm(0,1e-5)
+phi~dgamma(0.01,0.01)
 #2. Likelihood
-for (i in 1:N){
-Y[i] ~ dbern(pi[i])
-logit(pi[i]) <-  eta[i]
-eta[i] <- beta.0+beta.1*mag_g[i]+beta.2*bar[i]+ranef[galtype[i]]
-#eta[i] <- beta.0+beta.1*mag_g[i]+ranef[galtype[i]]
+for(i in 1:N){
+logit(mu[i]) <- b0 + b1*x[i]
+#   sigma[i]<-exp(c0+c1*x[i])
+
+#  p[i] <-max(0.000001,((1-mu[i])*mu[i]*mu[i]-mu[i]*sigma[i]^2)/sigma[i])
+#  q[i] <- max(0.00001,(1-mu[i])*(mu[i]-mu[i]*mu[i]-sigma[i])/sigma[i])
+
+p[i]<-mu[i]*phi
+q[i]<-(1-mu[i])*phi
+  Y[i] ~ dbeta(p[i],q[i])
+
 #3. Prediction
-prediction[i]~dbern(pi[i])
+prediction[i]~dbeta(p[i],q[i])
 }
 }"
+
+params <- c("b0","b1","prediction")
+
+inits1=list(b0=rnorm(1,0,1),b1=rnorm(1,0,1),c0=rnorm(1,0,1),c1=rnorm(1,0,1))
+inits2=list(b0=rnorm(1,0,1),b1=rnorm(1,0,1),c0=rnorm(1,0,1),c1=rnorm(1,0,1))
+inits3=list(b0=rnorm(1,0,1),b1=rnorm(1,0,1),c0=rnorm(1,0,1),c1=rnorm(1,0,1))
+
+library(parallel)
+cl <- makeCluster(3)
+jags.logit <- run.jags(method="rjparallel", 
+                       data = jags.data, 
+                       inits = list(inits1,inits2,inits3),
+                       model=model,
+                       n.chains = 3,
+                       adapt=1500,
+                       monitor=c(params),
+                       burnin=1000,
+                       sample=5000,
+                       summarise=FALSE,
+                       plots=FALSE
+)
+
+jagssamples <- as.mcmc.list(jags.logit)
+summary<-extend.jags(jags.logit,drop.monitor=c("prediction"), summarise=TRUE)
+print(summary)
+
+# Diagnostics Confusion Matrix (very unlikely to be good)
+predscape<-summary(as.mcmc.list(jags.logit, vars="prediction"))
+predscape<-predscape$quantiles
+
+
+plot(x,predscape[,3])
+plot(x,data.3$fEsc)
