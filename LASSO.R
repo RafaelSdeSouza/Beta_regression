a834aac3deaf7ad21cbb059eacb9876b4cac45a1
Rafael S. de Souza <rafael.2706@gmail.com>
2015-09-24 20:00:00 +0200

    LASSO

--- LASSO.R
+++ LASSO.R
@@ -23,6 +23,7 @@ library(arm)
 require(gam)
 require(glmnet)
 require(AMADA)
+require(mgcv)
 
 
 
@@ -32,75 +33,67 @@ colnames(data.1)<-c("redshift","fEsc","Mvir","Mstar","Mgas","QHI","sfr_gas",
                     "sfr_stars","ssfr_gas","ssfr_stars","baryon_fraction",
                     "spin","age_star_mean","age_star_max","age_star_min","NH_10")
 
+data.1$Mvir<-log(data.1$Mvir,10)
+data.1$Mstar<-log(data.1$Mstar,10)
+data.1$Mgas<-log(data.1$Mgas,10)
 
 #trainIndex <- createDataPartition(data.1$redshift, p = .25,
 #                                  list = FALSE,
 #                                  times = 1)
-data.2<-data.1[data.1$redshift<=15,]
+data.2<-data.1[data.1$redshift<=20,]
 
 
 # Exploratory plot AMADA correlations 
 
 
-cor1<-Corr_MIC(data.2[,-2],method="pearson")
-plotdendrogram(cor1,type="p")
+#cor1<-Corr_MIC(data.2[,-2],method="pearson")
+#plotdendrogram(cor1,type="p")
 
+# remove collinearity 
 
-vifcor(data.2[,c(-1,-2)],th=0.80)
+VIF<-vifcor(data.2[,c(-1,-2)],th=0.7)
+remain<-levels(VIF@results$Variables)
 
+data.3<-as.data.frame(scale(data.2[,remain]))
 
-
-
-
-
-
-#data.2<-data.1[trainIndex,]
-#data.2<-data.1[data.1$redshift==8.86815,]
-#data.2<-data.1
 N<-nrow(data.2)
+data.3$Y<-(data.2$fEsc*(N-1)+0.5)/N
 
+# Reionization cutoff
 
+cut<-0.5
 
-data.2$Y<-(data.2$fEsc*(N-1)+0.5)/N
-data.2$Y[data.2$Y>=0.5]<-1
-data.2$Y[data.2$Y<0.5]<-0
+data.3$Y[data.3$Y>=cut]<-1
+data.3$Y[data.3$Y<cut]<-0
 #data.2$Y<-as.factor(data.2$Y)
 
 
+# Fit LASSO binomial regression 
 
-# Prepare data 
-data.2$Mstar<-(data.2$Mstar-mean(data.2$Mstar))/sd(data.2$Mstar)
-data.2$Mgas<-(data.2$Mgas-mean(data.2$Mgas))/sd(data.2$Mgas)
-data.2$Mvir<-(data.2$Mvir-mean(data.2$Mvir))/sd(data.2$Mvir)
-data.2$sfr_gas<-(data.2$sfr_gas-mean(data.2$sfr_gas))/sd(data.2$sfr_gas)
-data.2$ssfr_stars<-(data.2$ssfr_stars-mean(data.2$ssfr_stars))/sd(data.2$ssfr_stars)
-data.2$baryon_fraction<-(data.2$baryon_fraction-mean(data.2$baryon_fraction))/sd(data.2$baryon_fraction)
-data.2$QHI<-(data.2$QHI-mean(data.2$QHI))/sd(data.2$QHI)
-data.2$ssfr_gas<-(data.2$ssfr_gas-mean(data.2$ssfr_gas))/sd(data.2$ssfr_gas)
-data.2$age_star_mean<-(data.2$age_star_mean-mean(data.2$age_star_mean))/sd(data.2$age_star_mean)
-data.2$spin<-(data.2$spin-mean(data.2$spin))/sd(data.2$spin)
-data.2$NH_10<-(data.2$NH_10-mean(data.2$NH_10))/sd(data.2$NH_10)
-data.2$sfr_stars<-(data.2$sfr_stars-mean(data.2$sfr_stars))/sd(data.2$sfr_stars)
-data.2$age_star_max<-(data.2$age_star_max-mean(data.2$age_star_max))/sd(data.2$age_star_max)
-data.2$age_star_min<-(data.2$age_star_min-mean(data.2$age_star_min))/sd(data.2$age_star_min)
+x2<-as.matrix(data.3[,remain])
 
-x2<-as.matrix(data.2[,c("Mstar","Mgas","ssfr_gas","ssfr_stars","baryon_fraction","spin","age_star_max","age_star_min","NH_10")])
-
-fit<-glmnet(x2,y=data.2$Y,alpha=1,family="binomial")
+fit<-glmnet(x2,y=data.3$Y,alpha=1,family="binomial")
 plot(fit,xvar="lambda",label = TRUE)
 plot(fit, xvar = "dev", label = TRUE)
-cv.glmmod <- cv.glmnet(x2,y=data.2$Y,alpha=1,family="binomial",type.measure = "auc")
-plot(cv.glmmod)
+cv.glmmod <- cv.glmnet(x2,y=data.3$Y,alpha=1,family="binomial",type.measure = "auc")
+
+# Plot AUC vs Lambda
+plot(cv.glmmod,xlab=expression(log~lambda),family="serif")
+
+
 best_lambda <- cv.glmmod$lambda.min
-coef.min = coef(cv.glmmod, s = "lambda.min")
-active.min = coef.min[which(abs(coef.min) > 0.05)]
-index.min = coef.min[active.min]
+coef.min = coef(cv.glmmod, s = "lambda.1se")
+coef.min
 
 
 
-fit=glm(Y~Mvir+baryon_fraction,data=data.2,family=binomial("probit"))
+# Fit GLM with selected features
+
+
+
+fit=glm(Y~Mgas +ssfr_stars+age_star_min ,data=data.2,family=binomial("probit"))
 ROCtest(fit,10,"ROC")
-fit2=gam(Y~te(QHI,baryon_fraction),data=data.2,family=binomial("logit"))
+fit2=gam(Y~te(Mgas,ssfr_stars),data=data.2,family=binomial("logit"))
 plot(fit2)
 vis.gam(fit2,type="response",plot.type = "persp",color="topo", border=NA, n.grid=500,theta=-60,phi=30)
 ROCtest(fit2,10,"ROC")
@@ -127,3 +120,23 @@ ggplot(mydata) +
   geom_line(aes(ID, `0.975quant`), linetype="dashed")
 
 model.mcmc = MCMClogit(Y~QHI+baryon_fraction,data=data.2,, mcmc=5000)
+
+
+
+
+
+# Prepare data 
+#data.2$Mstar<-(data.2$Mstar-mean(data.2$Mstar))/sd(data.2$Mstar)
+#data.2$Mgas<-(data.2$Mgas-mean(data.2$Mgas))/sd(data.2$Mgas)
+#data.2$Mvir<-(data.2$Mvir-mean(data.2$Mvir))/sd(data.2$Mvir)
+#data.2$sfr_gas<-(data.2$sfr_gas-mean(data.2$sfr_gas))/sd(data.2$sfr_gas)
+#data.2$ssfr_stars<-(data.2$ssfr_stars-mean(data.2$ssfr_stars))/sd(data.2$ssfr_stars)
+#data.2$baryon_fraction<-(data.2$baryon_fraction-mean(data.2$baryon_fraction))/sd(data.2$baryon_fraction)
+#data.2$QHI<-(data.2$QHI-mean(data.2$QHI))/sd(data.2$QHI)
+#data.2$ssfr_gas<-(data.2$ssfr_gas-mean(data.2$ssfr_gas))/sd(data.2$ssfr_gas)
+#data.2$age_star_mean<-(data.2$age_star_mean-mean(data.2$age_star_mean))/sd(data.2$age_star_mean)
+#data.2$spin<-(data.2$spin-mean(data.2$spin))/sd(data.2$spin)
+#data.2$NH_10<-(data.2$NH_10-mean(data.2$NH_10))/sd(data.2$NH_10)
+#data.2$sfr_stars<-(data.2$sfr_stars-mean(data.2$sfr_stars))/sd(data.2$sfr_stars)
+#data.2$age_star_max<-(data.2$age_star_max-mean(data.2$age_star_max))/sd(data.2$age_star_max)
+#data.2$age_star_min<-(data.2$age_star_min-mean(data.2$age_star_min))/sd(data.2$age_star_min)
