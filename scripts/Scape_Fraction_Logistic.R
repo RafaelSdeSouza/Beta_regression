e456d41d9d367f9923861c73dfd92c78a075fff2
Rafael S. de Souza <rafael.2706@gmail.com>
2016-06-14 18:31:55 +0200

    update logit script

--- scripts/Scape_Fraction_Logistic.R
+++ scripts/Scape_Fraction_Logistic.R
@@ -14,26 +14,13 @@
 #  http://www.r-project.org/Licenses/
 #
 #  Required libraries
-library(rjags)
+require(R2jags)
 library(ggmcmc)
 library(ggplot2)
 library(ggthemes)
-library(pander)
-library(Cairo)
-library(plyr)
-library(MASS)
-library(scales)
-library(plyr)
-require(gdata)
-require(runjags)
-require(gdata)
-require(caret)
-require(pROC)
-require(plyr)
-
 #Read the  dataset
 
-data.1= read.table(file="FiBY_escape_data_all.dat",header=FALSE)
+data.1= read.table(file="..//data/FiBY_escape_data_all.dat",header=FALSE)
 colnames(data.1)<-c("redshift","fEsc","Mvir","Mstar","Mgas","QHI","sfr_gas",
                     "sfr_stars","ssfr_gas","ssfr_stars","baryon_fraction",
                     "spin","age_star_mean","age_star_max","age_star_min","NH_10")
@@ -42,25 +29,15 @@ data.2<-data.1[data.1$redshift<=10,]
 N<-nrow(data.2)
 
 
-
-data.2$Y<-(data.2$fEsc*(N-1)+0.5)/N
-data.2$Y[data.2$Y>=0.1]<-1
-data.2$Y[data.2$Y<0.1]<-0
+data.2$Y[data.2$fEsc>=0.1]<-1
+data.2$Y[data.2$fEsc<0.1]<-0
 
 
 # Prepare data for JAGS
-data.2$Mstar<-(data.2$Mstar-mean(data.2$Mstar))/sd(data.2$Mstar)
-data.2$Mgas<-(data.2$Mgas-mean(data.2$Mgas))/sd(data.2$Mgas)
-data.2$Mvir<-(data.2$Mvir-mean(data.2$Mvir))/sd(data.2$Mvir)
-data.2$sfr_gas<-(data.2$sfr_gas-mean(data.2$sfr_gas))/sd(data.2$sfr_gas)
-data.2$baryon_fraction<-(data.2$baryon_fraction-mean(data.2$baryon_fraction))/sd(data.2$baryon_fraction)
-data.2$QHI<-(data.2$QHI-mean(data.2$QHI))/sd(data.2$QHI)
-data.2$ssfr_gas<-(data.2$ssfr_gas-mean(data.2$ssfr_gas))/sd(data.2$ssfr_gas)
-data.2$age_star_mean<-(data.2$age_star_mean-mean(data.2$age_star_mean))/sd(data.2$age_star_mean)
-data.2$spin<-(data.2$spin-mean(data.2$spin))/sd(data.2$spin)
-data.2$NH_10<-(data.2$NH_10-mean(data.2$NH_10))/sd(data.2$NH_10)
+x.scale<-as.data.frame(scale(data.2[,c("Mstar","Mgas","Mvir","sfr_gas","baryon_fraction","ssfr_gas","age_star_mean",
+                        "spin","NH_10","QHI")]))
 
-X<-model.matrix(~baryon_fraction+QHI,data=data.2)
+X<-model.matrix(~baryon_fraction+QHI,data=x.scale)
 
 # Scale
 K<-ncol(X)
@@ -75,16 +52,9 @@ jags.data <- list(Y= data.2$Y,
 )
 
 
-model<-"model{
+LOGIT<-"model{
 #1. Priors 
-#beta~dmnorm(b0[],B0[,]) # Normal Priors
-# Jefreys priors for sparseness 
-for(j in 1:Npred)   {
-      lnTau[j] ~ dunif(-50, 50)   
-      TauM[j] <- exp(lnTau[j])
-     beta[j] ~ dnorm(0, TauM[j]) 
-    Ind[j] <- step(abs(beta[j]) - 0.05)
-}
+beta~dmnorm(b0[],B0[,]) # Normal Priors
 
 #2. Likelihood
 
@@ -100,98 +70,21 @@ NewPred[i]~dbern(pi[i])
 
 }"
 
-params <- c("beta","pi","Ind","NewPred")
+params <- c("beta","pi","NewPred")
 
-inits0  <- function () {
+inits  <- function () {
   list(beta = rnorm(K, 0, 0.1))}
 
-
-
-inits1=inits0()
-inits2=inits0()
-inits3=inits0()
-
-library(parallel)
-cl <- makeCluster(3)
-jags.logit <- run.jags(method="rjparallel", 
-                       data = jags.data, 
-                       inits = list(inits1,inits2,inits3),
-                       model=model,
-                       n.chains = 3,
-                       adapt=1000,
-                       monitor=c(params),
-                       burnin=1500,
-                       sample=5000,
-                       summarise=FALSE,
-                       plots=FALSE
-)
-
-jagssamples <- as.mcmc.list(jags.logit)
-#summary<-extend.jags(jags.logit,drop.monitor=c("pi","prediction"), summarise=TRUE)
-#print(summary)
-
-L.factors <- data.frame(
-  Parameter=paste("beta[", seq(1:5), "]", sep=""),
-  Label=c("(Intercept)","Mvir","baryon_fraction","age_star_mean","ssfr_gas"))
-beta_post<-ggs(jagssamples,par_labels=L.factors,family=c("beta"))
-
-pdf("beta_scape.pdf")
-ggs_caterpillar(beta_post)+theme_hc()+ylab("")+geom_vline(xintercept=c(0,0), linetype="dotted")+
-theme(legend.position="none",plot.title = element_text(hjust=0.5),
-        axis.title.y=element_text(vjust=0.75),axis.text.x=element_text(size=25),
-        strip.text.x=element_text(size=25),
-        axis.title.x=element_text(vjust=-0.25),
-        text = element_text(size=20),axis.title.x=element_text(size=rel(1)))
-dev.off()
+jags.logit <- jags( 
+                    data = jags.data, 
+                    inits      = inits,
+                    parameters = params,
+                    model      = textConnection(LOGIT),
+                    n.chains   = 3,
+                    n.iter     = 500,
+                    n.thin     = 1,
+                    n.burnin   = 250)
 
 
 
-
-prediction<-summary(as.mcmc.list(jags.logit,vars="NewPred"))
-prediction<-prediction$quantiles
-
-probability<-summary(as.mcmc.list(jags.logit,vars="pi"))
-prob<-probability$quantiles
-
-ROC<-roc(as.numeric(prob[,3]), data.2$Y)
-
-plot(ROC)
-require(mlearning)
-require(caret)
-
-xtab <- table(prediction[,3], data.2$Y)
-confusionMatrix(xtab)
-
-
-
-# Diagnostics Confusion Matrix (very unlikely to be good)
-predscape<-summary(as.mcmc.list(jags.logit, vars="newY"))
-predscape<-predscape$quantiles
-
-
-plot(data.2$fEsc,predscape[,3])
-plot(data.2$baryon_fraction,predscape[,3])
-plot(log(data.2$baryon_fraction,10),data.2$fEsc)
-
-
-
-fit=glm(Y~QHI+baryon_fraction,data=data.2,family=binomial("logit"))
-ROCtest(fit)
-
-hlGOF.test(data.2$Y, predict(fit,data.2, type="response"), breaks=12)
-ROC<-roc(predict(fit, type="response"), data.2$Y)
-ROC
-require(LOGIT)
-
-
-gdata<-data.frame(pi=predict(fit,type="resp"),x=data.2$QHI,y=data.2$Y)
-ggplot(gdata,aes(x=x,y=pi))+geom_line()+geom_point(data=gdata,aes(x=x,y=y))
-
-
-
-library(popbio)
-logi.hist.plot(data.2$Mvir ,data.2$Y,boxp=F,type="hist",counts = T,col="gray",xlabel = expression(f[gas]),ylabel="Probability")
-
-d1 <- genLogiDt(model=FALSE)
-f1 <- glm(y ~ I(x5^2)*x1 -1, family=binomial("logit"), data=d1)
-logiDx(f1)
+print(jags.logit,justify = "left", digits=2)
