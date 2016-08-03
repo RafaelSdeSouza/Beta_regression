9c8fd3a9867d68756ebbfe8cd57e4fe102ec1d46
Rafael <rafael.2706@gmail.com>
2016-08-03 05:45:22 +0200

    e

---
+++ scripts/Code2.R
@@ -0,0 +1,111 @@
+rm(list=ls(all=TRUE))
+library(caret);library(mgcv);library(ggplot2);library(e1071);library(corrplot);library(reshape)
+Data=read.table("..//data/FiBY_escape_data_all.dat.txt",header=F)
+data.1 = Data
+colnames(data.1)<-c("redshift","fEsc","Mvir","Mstar","Mgas","QHI","sfr_gas",
+                    "sfr_stars","ssfr_gas","ssfr_stars","baryon_fraction",
+                    "spin","age_star_mean","age_star_max","age_star_min","NH_10")
+data.2 = data.1[data.1$redshift<=10,]
+## fEsc is the variable of interest
+Data <- as.data.frame(data.2[,c("Mstar","Mgas","Mvir","sfr_gas","baryon_fraction","ssfr_gas","age_star_mean","spin","NH_10","QHI")])
+X    <- as.matrix(Data)
+y    <- data.2$fEsc; 
+y[ y < 10^-2] = 0
+
+# Transform the columns of the design matrix (X)  to lighten the skewness, reduce the effect of outliers and reduce pairwise correlations if possible
+trans       <- preProcess(X,method = c("YeoJohnson", "center", "scale","spatialSign")) # Yeo-Johnson followed by centering and scaling. "spatialSign" is a bit complicated but it seems useful here
+                                                                                       # to reduce outliers. Also I noticed that many of the variables are highly concetrated at one point. "spatailSign" will
+                                                                                       # distribute things around.  
+Xtrans      <- predict(trans,X) # The "new" Transformed X                 
+## Check skewness Before and After transformation
+apply(X,2,function(x) skewness(x)) # All are exteremly skewed except perhaps age_star_mean
+apply(Xtrans,2,function(x) skewness(x)) # Much Improved. 
+## Check boxplots
+ggplot(data=melt(as.data.frame(scale(X))), aes(variable, value)) + geom_boxplot()
+ggplot(data=melt(as.data.frame(Xtrans)), aes(variable, value)) + geom_boxplot()
+
+### Two Models: 1) Model the probability that y > 0. 2) Model the Average of Y if y > 0 
+### Some graphics:
+### Plot the data (transformed) with a smoother:
+### Good practice to go through all predictors (notice non-linearity):
+non.zero <- ifelse(y > 0, 1, 0)
+d        <- data.frame(Xtrans,y=y,non.zero)
+ggplot(d, aes(x=Mstar, y=y, colour = as.factor(non.zero))) + geom_point(size=3,alpha = .5,pch=20) + geom_smooth(lwd=1.5,col="Blue")+ theme_bw()
+ggplot(d, aes(x=sfr_gas, y=y, colour = as.factor(non.zero))) + geom_point(size=3,alpha = .5,pch=20) + geom_smooth(lwd=1.5,col="Blue")+ theme_bw()
+ggplot(d, aes(x=NH_10, y=y, colour = as.factor(non.zero))) + geom_point(size=3,alpha = .5,pch=20) + geom_smooth(lwd=1.5,col="Blue")+ theme_bw() #  weird !
+
+####
+#### Modelling:
+#### Using Non-parametric regression function we will adopt more general regression functions without restricting to specific functional assumption 
+#### 1) Model Prob(y>0)
+mod_linear_nzero   <- gam(non.zero ~Mstar + Mgas + Mvir + sfr_gas + baryon_fraction + ssfr_gas + age_star_mean + spin + NH_10 + QHI, data = d, family = binomial(link = logit))
+r                  <- 30
+M_non.zero         <- gam(non.zero ~ s(Mstar,bs="cr",k=r)    + s(Mgas,bs="cr",k=r) + s(Mvir,bs="cr",k=r) + s(sfr_gas,bs="cr",k=r) + s(baryon_fraction,bs="cr",k=r) +
+                    s(ssfr_gas,bs="cr",k=r) + s(age_star_mean,bs="cr",k=r) + s(spin,bs="cr",k=r) + s(NH_10,bs="cr",k=r) + s(QHI,bs="cr",k=r),
+                    data=d,family=binomial(link="logit"))
+
+anova.gam(mod_linear_nzero,M_non.zero,test="Chisq") # Test the simple model against the more complicated one
+summary(M_non.zero)
+plot(M_non.zero,pages=1,residuals=F,scheme=1,rug=FALSE,lwd=3,shade=TRUE,seWithMean=TRUE) # Non-linearity does not seem obvious for the some of the variables 
+gam.check(M_non.zero) # Residual analysis
+
+
+### 2) Model Average y when y > 0. 
+## Competing models     1) log normal (simple but can produce predicted values greater than 1)
+##                      2) beta    (correct scale but more complicated) 
+##                      3) Gamma models (can handle skewness naturally)
+# Log normal
+M_L_y    <- gam(log(y) ~ s(Mstar,bs="cr",k=100)    + s(Mgas,bs="cr",k=100) + s(Mvir,bs="cr",k=100) + s(sfr_gas,bs="cr",k=100) + s(baryon_fraction,bs="cr",k=100) +
+                s(ssfr_gas,bs="cr",k=100) + s(age_star_mean,bs="cr",k=100) + s(spin,bs="cr",k=100) + s(NH_10,bs="cr",k=100) + s(QHI,bs="cr",k=100),
+                subset=y>0,data=d,gamma=1.4)
+
+summary(M_L_y)
+plot(M_L_y,pages=1,residuals=F,scheme=1,rug=FALSE,lwd=3,shade=TRUE,seWithMean=TRUE) 
+gam.check(M_L_y) # Residual analysis
+
+
+# Beta  
+M_Beta_y <- gam(y ~ s(Mstar,bs="cr",k=100)    + s(Mgas,bs="cr",k=100) + s(Mvir,bs="cr",k=100) + s(sfr_gas,bs="cr",k=100) + s(baryon_fraction,bs="cr",k=100) +
+                    s(ssfr_gas,bs="cr",k=100) + s(age_star_mean,bs="cr",k=100) + s(spin,bs="cr",k=100) + s(NH_10,bs="cr",k=100) + s(QHI,bs="cr",k=100),
+                     subset=y>0,data=d,family=betar(link="logit"),gamma=1.4)
+
+summary(M_Beta_y)
+plot(M_Beta_y,pages=1,residuals=F,scheme=1,rug=FALSE,lwd=3,shade=TRUE,seWithMean=TRUE) 
+gam.check(M_Beta_y) # Residual analysis
+
+
+
+# Gamma 
+M_Gamma_y <- gam(y ~ s(Mstar,bs="cr",k=100)    + s(Mgas,bs="cr",k=100) + s(Mvir,bs="cr",k=100) + s(sfr_gas,bs="cr",k=100) + s(baryon_fraction,bs="cr",k=100) +
+                     s(ssfr_gas,bs="cr",k=100) + s(age_star_mean,bs="cr",k=100) + s(spin,bs="cr",k=100) + s(NH_10,bs="cr",k=100) + s(QHI,bs="cr",k=100),
+                     subset=y>0,data=d,family=Gamma(link="log"),gamma=1.4)
+
+summary(M_Gamma_y)
+plot(M_Gamma_y,pages=1,residuals=F,scheme=1,rug=FALSE,lwd=3,shade=TRUE,seWithMean=TRUE) 
+gam.check(M_Gamma_y) # Residual analysis
+
+
+#### 
+#### Prediction :
+#### Say at xp = X[c(100,363,987),] 
+xp= X[c(100,363,987),]; xp=as.data.frame(xp);colnames(xp) = colnames(X)
+xp.trans <- predict(trans,xp) # The "new" Transformed xp      
+# Find pr(y = 0) at xp 
+predict(M_non.zero,newdata=xp.trans,type="response")
+# Predict the average of y at xp: 
+predict(M_non.zero,newdata=xp.trans,type="response")*predict(M_Beta_y,newdata=xp.trans,type="response")
+
+
+
+## Fitted values:
+fit = predict(M_non.zero,newdata=as.data.frame(Xtrans),type="response")*predict(M_Beta_y,newdata=as.data.frame(Xtrans),type="response")
+cor(fit,y)
+plot(fit,y,pch=20,cex=0.5)
+abline(0,1,lty=2,col="Black",lwd=3)
+
+
+
+
+
+
+
