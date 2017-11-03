e33c4cb14c1f01bade4ed690e19efaebabc1a199
Rafael S. de Souza <rafael.2706@gmail.com>
2017-11-03 08:24:29 -0400

    w

--- scripts/plot_gam.R
+++ scripts/plot_gam.R
@@ -9,8 +9,17 @@ for( i in 1:10) lines(K$x, K[,paste0("B.",i)],lwd=2,lty=2)
 
 g_data <- melt(K,id.vars = "x")
 
+pdf("hx.pdf",width = 9,height = 8)
 ggplot(g_data,aes(x = x,y = value,group = variable,linetype = variable,color=variable)) +
   geom_line(size=1.25) + scale_linetype_manual(name="",values=c("solid","dashed",rep("dotted",10))) +
-  scale_color_manual(name="",values=c("green","red",rep("gray",10))) +
-  theme_hc() + ylab("h(x)") + xlab("x")
-  
+  scale_color_manual(name="",values=c("#3698BF","#A63921",rep("#D9D384",10))) +
+  scale_size_manual(name="",values=c(1.5,1.25,rep(1,10))) +
+  theme_economist_white() + ylab("h(x)") + xlab("x") + 
+  theme(legend.background = element_rect(fill="white"),
+        legend.key = element_rect(fill = "white",color = "white"),
+        plot.background = element_rect(fill = "white"),
+        legend.position="none",
+#        axis.title.y = element_text(vjust = 0.1,margin=margin(0,10,0,0)),
+#        axis.title.x = element_text(vjust = -0.25),
+        text = element_text(size = 25))
+dev.off()
