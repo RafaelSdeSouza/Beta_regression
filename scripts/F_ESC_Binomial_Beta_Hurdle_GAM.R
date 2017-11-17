be86177d175b2a76799679c8ecb7bdb56973066e
Rafael S. de Souza <rafael.2706@gmail.com>
2017-11-17 16:24:47 -0500

    d

--- scripts/F_ESC_Binomial_Beta_Hurdle_GAM.R
+++ scripts/F_ESC_Binomial_Beta_Hurdle_GAM.R
@@ -85,7 +85,7 @@ for(i in 1:p){
 # Plot  via ggplot2
 pdf("Binomial_GAM.pdf",width = 16,height = 8)
 ggplot(ggg_x,aes(x=x,y=Predictions))+
-  geom_hex(data=ggg_original,bins = 75,aes(x=x,y=y))+
+  geom_hex(data=ggg_original,alpha=0.25,bins = 75,aes(x=x,y=y))+
   scale_fill_continuous(low = "#D9D3B4", high = "#441D0D", trans = log10_trans())+
   geom_ribbon(aes(ymin=CI_L, ymax=CI_R),fill = c("#3698BF"),alpha=0.75) +
   geom_line(col="#D97C2B",size=0.75)+
@@ -158,7 +158,7 @@ for(i in 1:p){
 # Plot  via ggplot2
 pdf("Beta_GAM.pdf",width = 16,height = 8)
 ggplot(ggg_x,aes(x=x,y=Predictions))+
-  geom_hex(data=ggg_original,bins = 75,aes(x=x,y=y))+
+  geom_hex(data=ggg_original,alpha=0.25,bins = 75,aes(x=x,y=y))+
   scale_fill_continuous(low = "#D9D3B4", high = "#441D0D", trans = log10_trans())+
   geom_ribbon(aes(ymin=CI_L, ymax=CI_R),fill = c("#3698BF"),alpha=0.75) +
   geom_line(col="#D97C2B",size=0.75)+
@@ -229,8 +229,8 @@ for(i in 1:p){
 # Plot  via ggplot2
 pdf("Hurdle_GAM.pdf",width = 16,height = 8)
 ggplot(ggg_x,aes(x=x,y=Predictions))+
-  geom_hex(data=ggg_original,bins = 75,aes(x=x,y=y))+
-  geom_line(aes(x=x,y=SD),size=1.25)+
+  geom_hex(data=ggg_original,alpha=0.25,bins = 75,aes(x=x,y=y))+
+#  geom_line(aes(x=x,y=SD),size=1.25)+
   scale_fill_continuous(low = "#D9D3B4", high = "#441D0D", trans = log10_trans())+
   geom_ribbon(aes(ymin=CI_L, ymax=CI_R),fill = c("#3698BF"),alpha=0.75) +
   geom_line(col="#D97C2B",size=0.75)+
